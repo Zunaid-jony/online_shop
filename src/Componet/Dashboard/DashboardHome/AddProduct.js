0bdc2b239cbf6ca587fa0cd83a0062fa9d637927
Zunaid-jony <jony35666666@gmail.com>
2022-06-21 14:08:09 +0600

    file upload

--- src/Componet/Dashboard/DashboardHome/AddProduct.js
+++ src/Componet/Dashboard/DashboardHome/AddProduct.js
@@ -1,62 +1,114 @@
 import React from 'react';
-import { Box, MenuItem, TextField } from '@mui/material';
+import { TextField, Input, MenuItem } from '@mui/material';
+import { Button } from '@mui/material';
+import { useState } from 'react';
+
 const currencies = [
-    {
-      value: 'USD',
-      label: '$',
-    },
-    {
-      value: 'EUR',
-      label: '€',
-    },
-    {
-      value: 'BTC',
-      label: '฿',
-    },
-    {
-      value: 'JPY',
-      label: '¥',
-    },
-  ];
+  {
+    value: 'USD',
+    label: 'Burger',
+  },
+  {
+    value: 'EUR',
+    label: 'Pizza',
+  },
+  {
+    value: 'BTC',
+    label: 'Hotdog',
+  },
+  {
+    value: 'JPY',
+    label: 'Taco',
+  },
+  {
+    value: 'JPY',
+    label: 'Snack',
+  },
+  {
+    value: 'JPY',
+    label: 'Drink',
+  },
+];
 
 const AddProduct = () => {
-    const [currency, setCurrency] = React.useState('EUR');
+// const [name, setName] = useState('');
+const [ itemId, setItemId] = useState('');
+const [imgSrc, setImgSrc] = useState(null);
+const [id, setId] = useState('');
+
+const [name, setName] = useState('');
 
   const handleChange = (event) => {
-    setCurrency(event.target.value);
+    setName(event.target.value);
   };
-    return (
-        <>
-           <Box
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <div>
-        <TextField
-          id="outlined-select-currency"
-          select
-          label="Select"
-          value={currency}
-          onChange={handleChange}
-          helperText="Please select your currency"
-        >
-          {currencies.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
-        
-      </div>
-      
-    </Box>
+
+  const handleSubmit = e =>{
+    e.preventDefault();
+    if(!imgSrc){
+      return
+    }
+    const formData = new FormData()
+    formData.append('name', name);
+    formData.append('itemId', itemId)
+    formData.append('imgSrc', imgSrc)
+    formData.append('id', id)
+    fetch('http://localhost:5000/menuItem', {
+  method: 'POST',
+  body: formData
+})
+.then(response => response.json())
+.then(result => {
+  console.log('Success:', result);
+})
+.catch(error => {
+  console.error('Error:', error);
+});
+  }
+  return (
+    <div>
      
-        </>
-    );
+      <h1>Add MenuItems</h1>
+      <form onSubmit={handleSubmit}>
+      <TextField
+  sx={{width: '40%'}}
+   label="name" 
+   onChange={e => console.log(e.target.value)}
+   type='name'
+   required
+   variant="standard" />
+   <br></br>
+        <br></br>
+  <TextField
+  sx={{width: '40%'}}
+   label="itemId" 
+   onChange={e => setItemId (e.target.value)}
+   type='name'
+   required
+   variant="standard" />
+   <br></br>
+
+  <TextField 
+  sx={{width: '40%'}}
+   label="id" 
+   onChange={e => setId (e.target.value)}
+   type='number'
+   variant="standard" />
+   <br></br>
+   <Input accept="image/*"
+   onChange={e => setImgSrc(e.target.files[0])}
+   sx={{width: '40%'}}
+     multiple type="file" />
+     <br></br>
+     <br></br>
+   <Button variant="contained"
+    sx={{width: '40%'}}
+    type="submit"
+    >
+    Upload
+  </Button>
+  </form>
+    </div>
+  );
 };
 
 export default AddProduct;
